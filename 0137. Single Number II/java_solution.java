be94c4ee74390ef0c4fcde3e79dd9fc8521c9ecd
God-Father-007 <70606126+God-Father-007@users.noreply.github.com>
2024-03-28 19:22:19 +0530

    leetcode problem 137, java solution

---
+++ 0137. Single Number II/java_solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if( nums.length == 1 ) { return nums[0]; }
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++) {
+            if( i == 0 && nums[i] != nums[i+1] ) { return nums[i]; }
+            else if( i == nums.length-1 && nums[i] != nums[i-1] ) { return nums[i]; }
+            if( nums[i] != nums[i+1] && nums[i] != nums[i-1] ) { return nums[i]; }
+        }
+        return 0;
+    }
+}
