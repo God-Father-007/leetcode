b1c1ed7257f4c1e4cdc66147513e240e621ed964
God-Father-007 <70606126+God-Father-007@users.noreply.github.com>
2024-04-06 09:51:56 +0530

    leetcode problem 160, java solution

---
+++ 0160. Intersection of Two Linked Lists/java_solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if( headA == null || headB == null ) { return null; }
+        ListNode p1 = headA;
+        while( p1 != null ) {
+            ListNode p2 = headB;
+            while( p2 != null ) {
+                if( p1 == p2 ) { return p1; }
+                p2 = p2.next;
+            }
+            p2 = headB;
+            p1 = p1.next;
+        }
+        return null;
+    }
+}
