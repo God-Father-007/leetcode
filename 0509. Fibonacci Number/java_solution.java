d7a7632de69f5ac3d11dcb8bfd32332b138a813c
God-Father-007 <70606126+God-Father-007@users.noreply.github.com>
2024-07-21 10:37:02 +0530

    leetcode problem 509, java solution

---
+++ 0509. Fibonacci Number/java_solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int N) {
+        if( N < 2 ) { return N; }
+        int a = 0, b = 1, c = 0;
+        for( int i=2;i<=N;i++ ) {
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
